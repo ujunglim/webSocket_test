90d6a24a7fd40e40be6c892f525346f1bd6e7a2d
v_linyouzhen <v_linyouzhen@baidu.com>
2023-03-22 18:58:11 +0800

    change to socketIO, emit new message with nickname

--- src/public/app.js
+++ src/public/app.js
@@ -1,42 +1,73 @@
-// alert('This is frontend');
+const socket = io(); // io함수는 자동으로 연결된 socket을 추가한다.
+
+const welcomeForm = document.querySelector('#welcome');
+const roomDiv = document.querySelector('#room');
+
 const msgList = document.querySelector('ul');
 const nicknameForm = document.querySelector('#nickname');
 const msgForm = document.querySelector('#msg');
 
-// string 형태로 바꿔주는 함수 (사용하는 클라이언트가 GO, JAVA일 수 도 있기때문에 JS Object형태로 보내면 안 되고, String형태로 보내 모든 언어에 맞출수 있게 한다!)
-const makeMsg = (type, payload) => {
-    return JSON.stringify({type, payload});
+let roomName;
+roomDiv.hidden = true;
+
+// ================= VIEW CONTROLLERS ====================
+const showRoom = () => {
+  roomDiv.hidden = false;
+  welcomeForm.hidden = true;
+
+  // add room title
+  const h3 = document.querySelector('h3');
+  h3.innerText = `Room ${roomName}`;
 }
 
-const handlSendPaylod = (e) => {
-    const type = e.target.id;
-    e.preventDefault();
-    const input = (type === "nickname" ? nicknameForm : msgForm).querySelector('input');
-    const inputValue = input.value;
-    socket.send(makeMsg(type, inputValue));
-    input.value = '';
+const addMessage = (message) => {
+  const ul = roomDiv.querySelector('ul');
+  const li = document.createElement('li');
+  li.innerText = message;
+  ul.appendChild(li);
 }
 
-nicknameForm.addEventListener('submit', handlSendPaylod)
-msgForm.addEventListener('submit', handlSendPaylod);
-// 서버로 연결!
-const socket = new WebSocket(`ws://${window.location.host}`); // 프론트,서버 연결성공! socket을 이용해 프론트에서 서버로 메세지 전송가능!
+// ================= HANDLER ====================
+const handleRoomSubmit = (e) => {
+  e.preventDefault();
+  const input = welcomeForm.querySelector('input');
+  socket.emit(
+    'enter_room',
+    input.value,
+    showRoom
+  )
+  roomName = input.value;
+  input.value = '';
+}
 
-socket.addEventListener('open', () => {
-    console.log('Connected to Server ✅');
-});
+const handleNicknameSubmit = (e) => {
+  e.preventDefault();
+  const input = nicknameForm.querySelector('input');
+  socket.emit('nickname', input.value);
+  input.value = '';
+}
 
-socket.addEventListener('message', (msg) => {
-    const li = document.createElement('li');
-    li.innerText = msg.data;
-    msgList.append(li);
-});
+const handleMsgSubmit = (e) => {
+  e.preventDefault();
+  const input = msgForm.querySelector('input');
+  // socket.emit('new_msg', input.value, (nickname, msg) => addMessage(`${nickname}: ${msg}`));
+  socket.emit('new_msg', roomName, input.value);
+  input.value = '';
+}
 
-socket.addEventListener('error', (err) => {
-    console.log('error: ', err)
+// ================= EVENT LISTENERS ====================
+socket.on('welcome', () => {
+  addMessage('someone joined!')
 })
 
-socket.addEventListener('close', () => {
-    console.log('Disconnected to server ❌')
+socket.on('bye', (user) => {
+  addMessage(`${user} left T T`)
 })
 
+socket.on('new_msg', (msg) => {
+  addMessage(msg);
+})
+
+welcomeForm.addEventListener('submit', handleRoomSubmit);
+nicknameForm.addEventListener('submit', handleNicknameSubmit);
+msgForm.addEventListener('submit', handleMsgSubmit);
